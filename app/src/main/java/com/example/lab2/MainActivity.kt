f1d5e0a4ee759c8be3e85f6e9637c04ad80d2455
gurami152 <gurami152@gmail.com>
2020-08-28 18:56:58 +0300

    Define and Draw Triangle

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.lab2
 
+import android.opengl.GLSurfaceView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+private lateinit var gLView: GLSurfaceView
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        gLView = MyGLSurfaceView(this)
+        setContentView(gLView)
+
+
+
     }
 }
